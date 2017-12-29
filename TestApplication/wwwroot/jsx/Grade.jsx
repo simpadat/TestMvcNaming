a227fc031e12bd10974cf9f72c083adf1832a22e
Simon Davies <padat@hotmail.com>
2017-12-30 12:54:25 +1300

    basic aspnetcore web project from template. Simple model added to Home/Index react JSX to test model serialisation

---
+++ TestApplication/wwwroot/jsx/Grade.jsx
@@ -0,0 +1,25 @@
+﻿class Grade extends React.Component {
+
+    componentWillMount() {
+        if (!this.props.data || !this.props.data.Id || this.props.data.Id <= 0)
+            return;
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div>
+                    <div> Id: {this.props.data.Id}</div>
+                    <div> Description: {this.props.data.Description}</div>
+                </div>
+
+                <div>
+                    <div> id: {this.props.data.id}</div>
+                    <div> description: {this.props.data.description}</div>
+                </div>
+            </div>
+        );
+    }
+
+};
